4921fe3490d7be40920f2de901ef67bc37583ec8
sdyproject <ehddlrhome@naver.com>
2025-05-09 23:34:18 +0900

    250509

---
+++ javacodingstudy/src/_250509/자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,19 @@
+package _250509;
+
+import java.util.Arrays;
+
+public class 자연수_뒤집어_배열로_만들기 {
+
+	public static void main(String[] args) {
+		long l =12345;
+		
+		int[] answer = new int[String.valueOf(l).length()];
+		int index=0;
+		for(int i =answer.length-1; i>=0; i-- ) {
+			answer[index++] += String.valueOf(l).charAt(i)-'0';
+		}
+		
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
