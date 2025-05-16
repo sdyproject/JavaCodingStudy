7c5ed72d93f9fd0aaea6d2e9f9895cce581e3145
sdyproject <ehddlrhome@naver.com>
2025-05-16 22:09:46 +0900

    250516

---
+++ javacodingstudy/src/_250516/문자열_다루기_기본.java
@@ -0,0 +1,32 @@
+package _250516;
+
+public class 문자열_다루기_기본 {
+
+	public static void main(String[] args) {
+		String s ="a234";
+		 boolean answer = true;
+		 
+//		 if(s.length()==4 && s.length()==6) {
+//			 for(int i =0; i<s.length(); i++) {
+//					char ch = s.charAt(i);
+//					if(!Character.isDigit(ch)) {
+//						answer=false;
+//					}
+//				}
+//		 }
+		
+//		 if(s.length()==4 && s.length()==6) {
+//			 
+//			 for(char ch : s.toCharArray()) {
+//					if(!Character.isDigit(ch)) {
+//						answer=;
+//						break;
+//					}
+//				}
+//		 }
+		 	System.out.println(Integer.parseInt(s));
+		 	
+		System.out.println(answer);
+	}
+
+}
---
+++ javacodingstudy/src/_250516/문자열을_정수로_바꾸기.java
@@ -0,0 +1,12 @@
+package _250516;
+
+public class 문자열을_정수로_바꾸기 {
+
+	public static void main(String[] args) {
+		
+		String s ="1234";
+		
+		System.out.println(Integer.parseInt(s));
+	}
+
+}
