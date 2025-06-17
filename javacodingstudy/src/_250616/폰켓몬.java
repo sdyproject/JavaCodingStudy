d8fa649ed1b9b4dd525bf576f24fc8133fe10d15
sdyproject <ehddlrhome@naver.com>
2025-06-17 22:57:11 +0900

    250617

---
+++ javacodingstudy/src/_250616/폰켓몬.java
@@ -0,0 +1,35 @@
+package _250616;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class 폰켓몬 {
+
+	public static void main(String[] args) {
+//		int[] nums = {3,1,2,3};
+		int[] nums = {3,3,3,2,2,2};
+        int answer = 0;
+        
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for(int num : nums) {
+//        	
+//        	map.put(num, map.getOrDefault(num, 0)+1);
+//        	
+//        }
+//        System.out.println(map);
+        
+        Set<Integer> set = new HashSet<>();
+        int count = nums.length/2;
+        for(int num : nums) {
+        	set.add(num);
+        }
+        System.out.println(set);
+        
+        answer = (set.size()>=count) ? count: set.size();
+        
+        System.out.println(answer);
+	}
+
+}
