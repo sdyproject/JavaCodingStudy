165d867fbaf96f0175426984af56bd6cf878700e
sdyproject <ehddlrhome@naver.com>
2025-08-01 22:22:25 +0900

    250801

---
+++ javacodingstudy/src/_250801/내적.java
@@ -0,0 +1,19 @@
+package _250801;
+
+public class 내적 {
+
+	public static void main(String[] args) {
+//		int[] a = {1,2,3,4}; int[] b= {-3,-1,0,2};
+		int[] a = {-1,0,1}; int[] b= {1,0,-1};
+		int answer = 0;
+		
+		
+		for(int  i = 0; i < a.length; i++) {
+			
+			answer+=a[i] * b[i];
+			
+		}
+		System.out.println(answer);
+	}
+
+}
