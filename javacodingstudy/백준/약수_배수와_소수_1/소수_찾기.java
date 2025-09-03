d47bfe5e826c879faa6abb88053a87a32756ce7a
sdyproject <ehddlrhome@naver.com>
2025-09-03 23:34:57 +0900

    250903

---
+++ javacodingstudy/백준/약수_배수와_소수_1/소수_찾기.java
@@ -0,0 +1,19 @@
+package 약수_배수와_소수_1;
+
+import java.util.Scanner;
+
+public class 소수_찾기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N =sc.nextInt();
+		int count = 0;
+		for(int i = 0; i<N; i++) {
+			int num = sc.nextInt();
+			
+		}
+		System.out.println(count);
+	}
+
+}
