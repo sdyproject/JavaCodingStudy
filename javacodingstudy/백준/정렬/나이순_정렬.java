b558180ca22b6fbcaf103802e25f01a19ac46bff
sdyproject <ehddlrhome@naver.com>
2025-09-10 23:43:40 +0900

    250910

--- javacodingstudy/백준/정렬/나이순_정렬.java
+++ javacodingstudy/백준/정렬/나이순_정렬.java
@@ -1,8 +1,9 @@
 package 정렬;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,8 +14,16 @@ public class 나이순_정렬 {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
+		Map<String, Integer> map = new LinkedHashMap<>();
+		
+		for (int i = 0; i < N; i++) {
+			int age = sc.nextInt();
+			String name = sc.next();
+			map.put(name, age);
+		}
 		
 		
+		System.out.println(map);
 	}
 
 }
