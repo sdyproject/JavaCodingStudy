2d25e871f7bb2f4ff510436361ab191a3a3e2eb1
sdyproject <ehddlrhome@naver.com>
2025-09-29 21:43:27 +0900

    250929

---
+++ javacodingstudy/백준/조합론/녹색거탑.java
@@ -0,0 +1,15 @@
+package 조합론;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 녹색거탑 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println((int)Math.pow(N, 2));
+	}
+
+}
