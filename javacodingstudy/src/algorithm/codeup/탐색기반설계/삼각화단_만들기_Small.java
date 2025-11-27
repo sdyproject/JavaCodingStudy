11bb837d4b120ad498dea8b0f26f1f476c474827
sdyproject <ehddlrhome@naver.com>
2025-11-27 22:52:42 +0900

    2501127

---
+++ javacodingstudy/src/algorithm/codeup/탐색기반설계/삼각화단_만들기_Small.java
@@ -0,0 +1,16 @@
+package algorithm.codeup.탐색기반설계;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 삼각화단_만들기_Small {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int count =0;
+	
+	}
+
+	
+}
