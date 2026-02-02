bcb1b1e4f5f9c44a45c9f767c568d53787c889e9
sdyproject <ehddlrhome@naver.com>
2026-02-02 23:29:39 +0900

    260202

---
+++ javacodingstudy/src/algorithm/programmers/Lv0/최빈값_구하기.java
@@ -0,0 +1,20 @@
+package algorithm.programmers.Lv0;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 최빈값_구하기 {
+
+	//해결못함
+	public static void main(String[] args) {
+		int[] arrary = {1,2,3,3,3,4};
+		int max = 0;
+		int answer = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < arrary.length; i++) {
+			
+		}
+		
+	}
+
+}
