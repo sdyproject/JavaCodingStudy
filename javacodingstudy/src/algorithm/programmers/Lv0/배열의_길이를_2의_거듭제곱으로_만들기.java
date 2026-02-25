8a3bf4abe186f56b2f014d4e0a382ff202b15154
sdyproject <ehddlrhome@naver.com>
2026-02-25 23:26:47 +0900

    260225 Programmers Lv0

--- javacodingstudy/src/algorithm/programmers/Lv0/배열의_길이를_2의_거듭제곱으로_만들기.java
+++ javacodingstudy/src/algorithm/programmers/Lv0/배열의_길이를_2의_거듭제곱으로_만들기.java
@@ -10,10 +10,9 @@ public class 배열의_길이를_2의_거듭제곱으로_만들기 {
 //		int[] arr = {58, 172, 746, 89};
         int[] arr = {1, 2, 3, 4, 5, 6};
         int len = arr.length;
-        while(len % 2 == 0){
-            len /= 2;
-        }
-        System.out.println(len);
+        int[] answer = {};
+
+
 			
 	}
 
--- javacodingstudy/src/algorithm/programmers/Lv0/주사위_게임_3.java
+++ javacodingstudy/src/algorithm/programmers/Lv0/주사위_게임_3.java
@@ -13,7 +13,29 @@ public class 주사위_게임_3 {
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
+        int answer = 0;
         int[] num = {a,b,c,d};
+        Arrays.sort(num);
+
+        if(num[0] == num[3]){
+            answer = 1111*num[0];
+        } else if(num[1] == num[3]){
+            answer = (int) Math.pow(10*num[3]+num[0],2);
+        } else if(num[0] == num[2]) {
+            answer = (int) Math.pow(10 * num[3] + num[0], 2);
+        } else if (num[0] == num[1] && num[2] == num[3]) {
+            answer = (num[0]+num[2]) * Math.abs(num[0] - num[2]);
+        } else if (num[0] == num[1]) {
+            answer = num[2] * num[3];
+        } else if (num[1] == num[2]) {
+            answer = num[0] * num[3];
+        } else if (num[2] == num[3]) {
+            answer = num[0] * num[1];
+        } else {
+            answer = num[0];
+        }
+
+        System.out.println(answer);
 
 
     }
