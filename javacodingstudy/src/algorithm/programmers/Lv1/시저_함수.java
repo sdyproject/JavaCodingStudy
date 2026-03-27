84d43aa21fd105e0fa44a56950c49093aabafc5a
sdyproject <ehddlrhome@naver.com>
2026-03-27 22:43:52 +0900

    260327 Programmers

--- javacodingstudy/src/algorithm/programmers/Lv1/시저_함수.java
+++ javacodingstudy/src/algorithm/programmers/Lv1/시저_함수.java
@@ -3,16 +3,32 @@ package algorithm.programmers.Lv1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class 시저_함수 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String s = st.nextToken();
-        int n = Integer.parseInt(st.nextToken());
-
+        String s = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == ' ') {
+                sb.append(ch);
+            } else if (ch >= 'a' && ch <= 'z') {
+                if (ch + n > 'z') {
+                    sb.append((char) (ch + n - 26));
+                } else {
+                    sb.append((char) (ch + n));
+                }
+            } else if (ch >= 'A' && ch <= 'Z') {
+                if (ch + n > 'Z') {
+                    sb.append((char) (ch + n - 26));
+                } else {
+                    sb.append((char) (ch + n));
+                }
+            }
+        }
+        System.out.println(sb.toString());
     }
 
 }
