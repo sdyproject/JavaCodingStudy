6304f125f889533576ac508ecb345a897b7febf6
sdyproject <ehddlrhome@naver.com>
2026-04-06 23:28:55 +0900

    260406 Programmers

--- javacodingstudy/src/algorithm/programmers/Lv1/체육복.java
+++ javacodingstudy/src/algorithm/programmers/Lv1/체육복.java
@@ -3,14 +3,14 @@ package algorithm.programmers.Lv1;
 import java.util.Arrays;
 
 public class 체육복 {
-    // 실패
+
     public static void main(String[] args) {
-        int n = 5;
-        int[] lost = {2, 4};
-        int[] reserve = {1, 3, 5};
 //        int n = 5;
 //        int[] lost = {2, 4};
-//        int[] reserve = {3};
+//        int[] reserve = {1, 3, 5};
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {3};
 //        int n = 3;
 //        int[] lost = {3};
 //        int[] reserve = {1};
@@ -18,28 +18,44 @@ public class 체육복 {
 //        int n = 5;
 //        int[] lost = {1, 3};
 //        int[] reserve = {1, 2};
-
+        int answer = n - lost.length;
+        System.out.println(answer + "answer");
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        int answer = n - lost.length;
+
         for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer++;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(lost));
+        System.out.println(Arrays.toString(reserve));
+        System.out.println(answer);
+
+        for (int i = 0; i < lost.length; i++) {
+            if (lost[i] < 0) {
+                continue;
+            }
             int front = lost[i] - 1;
             int back = lost[i] + 1;
             for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    reserve[j] = 0;
-                    break;
+                if (reserve[j] < 0) {
+                    continue;
                 }
-
                 if (front == reserve[j] || back == reserve[j]) {
-                    reserve[j] = 0;
                     answer++;
+                    System.out.println(answer);
+                    reserve[j] = -1;
                     break;
                 }
             }
 
         }
-        System.out.print(answer);
+        System.out.println(answer);
     }
 
 }
--- javacodingstudy/src/algorithm/programmers/Lv1/카드_뭉치.java
+++ javacodingstudy/src/algorithm/programmers/Lv1/카드_뭉치.java
@@ -1,15 +1,42 @@
 package algorithm.programmers.Lv1;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class 카드_뭉치 {
     public static void main(String[] args) {
-        String[] cards1 = {"i", "drink", "water"};
-        String[] cards2 = {"want", "to"};
-        String[] goal = {"i", "want", "to", "drink", "water"};
-
-//        String[] cards1 = {"i", "water", "drink"};
+//        String[] cards1 = {"i", "drink", "water"};
 //        String[] cards2 = {"want", "to"};
 //        String[] goal = {"i", "want", "to", "drink", "water"};
 
+        String[] cards1 = {"i", "water", "drink"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+        String answer = "Yes";
 
+        Queue<String> que1 = new ArrayDeque<>();
+        Queue<String> que2 = new ArrayDeque<>();
+
+        for (int i = 0; i < cards1.length; i++) {
+            que1.add(cards1[i]);
+        }
+        for (int i = 0; i < cards2.length; i++) {
+            que2.add(cards2[i]);
+
+        }
+
+        for (String str : goal) {
+            if (!que1.isEmpty() && str.equals(que1.peek())) {
+                que1.poll();
+            } else if (!que2.isEmpty() && str.equals(que2.peek())) {
+                que2.poll();
+            } else {
+                answer = "No";
+                break;
+            }
+        }
+
+
+        System.out.println(answer);
     }
 }
