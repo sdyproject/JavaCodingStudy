81c0210f7e22e306adcd5f031f253afd95b8b3a9
sdyproject <ehddlrhome@naver.com>
2026-04-08 22:52:56 +0900

    260408 Programmers

--- javacodingstudy/src/algorithm/programmers/Lv1/달리기_경주.java
+++ javacodingstudy/src/algorithm/programmers/Lv1/달리기_경주.java
@@ -1,47 +1,31 @@
 package algorithm.programmers.Lv1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class 달리기_경주 {
 
     public static void main(String[] args) {
         String[] players = {"mumu", "soe", "poe", "kai", "mine"};
         String[] callings = {"kai", "kai", "mine", "mine"};
+        // 출력 ["mumu", "kai", "mine", "soe", "poe"]
 
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+        for (String call : callings) {
+            int rank = map.get(call);
+            String front = players[rank - 1];
+            players[rank - 1] = call;
+            players[rank] = front;
 
-// 시간 초과
-//		for(String str : callings) {
-//			for(int i =1; i<players.length; i++) {
-//				if(str.equals(players[i])) {
-//					String str3 = players[i];
-//					players[i] = players[i-1];
-//					players[i-1] = str3;
-//				}
-//			}
-//		}
-//		
-//		System.out.println(Arrays.toString(players));
+            map.put(call, rank - 1);
+            map.put(front, rank);
+        }
 
-//        String[] answer = players;
-//        Map<String, Integer> map = new HashMap<>();
-//
-//        for (int i = 0; i < players.length; i++) {
-//            map.put(players[i], i);
-//        }
-//
-//        for (String str : callings) {
-//            int rank = map.get(str);
-//            String name = players[rank - 1];
-////				System.out.println("name"+name);
-//
-//            answer[rank - 1] = str;
-//            answer[rank] = name;
-//            map.put(str, rank - 1);
-//            map.put(name, rank);
-//
-////		System.out.println(map.get(str));
-//        }
-//        System.out.println(Arrays.toString(answer));
-
-
+        System.out.println(Arrays.toString(players));
     }
 
 }
