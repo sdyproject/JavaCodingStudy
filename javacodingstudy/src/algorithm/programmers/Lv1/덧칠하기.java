4ba403500f3722719c798a86757e3d9d869aa800
sdyproject <ehddlrhome@naver.com>
2026-04-12 16:38:01 +0900

    260412 Programmers

---
+++ javacodingstudy/src/algorithm/programmers/Lv1/덧칠하기.java
@@ -0,0 +1,27 @@
+package algorithm.programmers.Lv1;
+
+public class 덧칠하기 {
+
+    public static void main(String[] args) {
+//        int n = 8;
+//        int m = 4;
+//        int[] section = {2, 3, 6};
+//        int n = 5;
+//        int m = 4;
+//        int[] section = {1, 3};
+        int n = 4;
+        int m = 1;
+        int[] section = {1, 2, 3, 4};
+        int answer = 0;
+        
+
+//        int num = 0;
+//        for (int i = 0; i < section.length; i++) {
+//            if (num <= section[i]) {
+//                num = section[i] + m;
+//                answer++;
+//            }
+//        }
+        System.out.println(answer);
+    }
+}
