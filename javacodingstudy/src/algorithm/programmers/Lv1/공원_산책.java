8cb02f3d0c0acb7472d8b9f1ab0c75529ebfc622
sdyproject <ehddlrhome@naver.com>
2026-04-16 22:12:44 +0900

    260416 Programmers

--- javacodingstudy/src/algorithm/programmers/Lv1/공원_산책.java
+++ javacodingstudy/src/algorithm/programmers/Lv1/공원_산책.java
@@ -5,64 +5,48 @@ import java.util.Arrays;
 public class 공원_산책 {
 
     public static void main(String[] args) {
-//        String[] park = {"SOO", "OOO", "OOO"};
-//        String[] routes = {"E 2", "S 2", "W 1"};
-//        String[] park = {"SOO", "OXX", "OOO"};
-//        String[] routes = {"E 2", "S 2", "W 1"};
-        String[] park = {"OSO", "OOO", "OXO", "OOO"};
-        String[] routes = {"E 2", "S 3", "W 1"};
 
-        int w = park[0].length();
-        int h = park.length;
+        String[] park = {"SOO", "OOO", "OOO"};
+        String[] routes = {"E 2", "S 2", "W 1"};
 
-        int startX = 0;
-        int startY = 0;
-        char[][] p_size = new char[h][w];
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                p_size[i][j] = park[i].charAt(j);
-                if (p_size[i][j] == 'S') {
-                    startY = i;
-                    startX = j;
-                }
+
+        int startx = 0;
+        int starty = 0;
+        char[][] ch = new char[park.length][park[0].length()];
+        for (int i = 0; i < park.length; i++) {
+            ch[i] = park[i].toCharArray();
+            if (park[i].contains("S")) {
+                startx = i;
+                starty = park[i].indexOf("S");
             }
         }
 
-        for (String route : routes) {
-            String way = route.split(" ")[0];
-            int step = Integer.valueOf(route.split(" ")[1]);
+        System.out.println(Arrays.deepToString(ch));
+        for (int i = 0; i < routes.length; i++) {
+            String way = routes[i].split(" ")[0];
+            int step = Integer.valueOf(routes[i].split(" ")[1]);
 
-            int nextY = startY;
-            int nextX = startX;
-
-            for (int i = 0; i < step; i++) {
+            for (int j = 0; j < step; j++) {
                 if (way.equals("E")) {
-                    nextY++;
+                    starty++;
                 } else if (way.equals("W")) {
-                    nextY--;
+                    starty--;
                 } else if (way.equals("S")) {
-                    nextX++;
+                    startx++;
                 } else {
-                    nextX--;
+                    startx--;
                 }
-                if (nextX >= 0 && nextY >= 0 && nextX < p_size.length && nextY < p_size[0].length) {
-                    if (p_size[nextX][nextY] == 'X') {
+
+                if (startx < ch.length && starty < ch[0].length) {
+                    if (ch[startx][starty] == 'X') {
                         break;
                     }
 
-                    if (i == step - 1) {
-                        startX = nextX;
-                        startY = nextY;
-                    }
                 }
             }
 
-
         }
-        int[] answer = new int[2];
-        answer[0] = startX;
-        answer[1] = startY;
-        System.out.println(Arrays.toString(answer));
-
+        System.out.println(startx);
+        System.out.println(starty);
     }
 }
