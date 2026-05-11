f9c9e5dce46a9ae8d01fae6e564b38fce4e92ffb
sdyproject <ehddlrhome@naver.com>
2026-05-11 22:55:55 +0900

    260511 Programmers

---
+++ javacodingstudy/src/algorithm/programmers/Lv2/주식가격.java
@@ -0,0 +1,12 @@
+package algorithm.programmers.Lv2;
+
+public class 주식가격 {
+    // 문제 해석 못함
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+
+        }
+    }
+}
